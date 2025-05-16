ae49f865fc62bf96f3529e1a851b0ab22ec5ff43
web22des <web22designer@yandex.ru>
2025-05-16 14:21:52 +0300

    add Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://web22des.github.io", // Ваш URL на GitHub Pages
-    base: "/astro-template_v-01/", // Если репозиторий НЕ username.github.io
+    //base: "/astro-template_v-01/", // Если репозиторий НЕ username.github.io
+    base: process.env.NODE_ENV === "production" ? "/astro-template_v-01/" : "/",
     outDir: "dist", // Должно быть именно 'dist'
 });
