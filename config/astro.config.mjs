23831f224b5e2b9f0aa6d718c6968f8caf02acf5
web22des <web22designer@yandex.ru>
2025-05-17 19:56:06 +0300

    add img

--- config/astro.config.mjs
+++ config/astro.config.mjs
@@ -1,5 +1,6 @@
 // config/astro/astro.config.mjs
 import { defineConfig } from "astro/config";
+//import { Image } from "astro:assets";
 import viteConfig from "./vite.config.js";
 
 const isDev = process.env.NODE_ENV === "development";
@@ -10,4 +11,5 @@ export default defineConfig({
     site: "https://web22des.github.io",
     base: isDev || isPreview || !isGHPages ? "/" : "/astro-template_v-01/",
     vite: viteConfig,
+    //integrations: [Image()],
 });
