2b8b39d372de6f90edd5bd7c2a316e321b789f1d
web22des <web22designer@yandex.ru>
2025-05-18 19:01:24 +0300

    fix

--- config/vite.config.js
+++ config/vite.config.js
@@ -18,16 +18,16 @@ export default {
         },
     },
     css: {
-        //postcss: path.resolve(__dirname, "../../config/postcss.config.js"),
-        postcss: path.resolve(__dirname, path.join(process.cwd(), "config", "postcss.config.js")),
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
-                @import "@styles/base/_variables.scss";
-                @import "@styles/base/mixins.scss";
-                `,
-            },
-        },
+        postcss: path.resolve(__dirname, "../../config/postcss.config.js"),
+        //postcss: path.resolve(__dirname, path.join(process.cwd(), "config", "postcss.config.js")),
+        // preprocessorOptions: {
+        //     scss: {
+        //         additionalData: `
+        //         @import "@styles/base/_variables.scss";
+        //         @import "@styles/base/mixins.scss";
+        //         `,
+        //     },
+        // },
     },
     build: {
         assetsInlineLimit: 0,
