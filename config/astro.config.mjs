9e37052ee00ca0a5c5620902fe7ac686e13ac3d4
web22des <web22designer@yandex.ru>
2025-05-18 21:05:58 +0300

    fix links

--- config/astro.config.mjs
+++ config/astro.config.mjs
@@ -2,24 +2,13 @@
 import { defineConfig } from "astro/config";
 import viteConfig from "./vite.config.js";
 
-// const isDev = process.env.NODE_ENV === "development";
-// const isPreview = process.argv.includes("preview");
-// const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
-
-// export default defineConfig({
-//     site: "https://web22des.github.io",
-//     base: isDev || isPreview || !isGHPages ? "/" : "/astro-template_v-01/",
-//     trailingSlash: "always",
-//     vite: viteConfig,
-// });
-
-// Для GitHub Pages используем имя репозитория как base
-const isGHPages = process.env.GITHUB_ACTIONS === "true";
-const base = isGHPages ? "/astro-template_v-01/" : "/";
+const isDev = process.env.NODE_ENV === "development";
+const isPreview = process.argv.includes("preview");
+const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
 
 export default defineConfig({
     site: "https://web22des.github.io",
-    base: base,
+    base: isDev || isPreview || !isGHPages ? "/" : "/astro-template_v-01/",
     trailingSlash: "always",
     vite: viteConfig,
 });
