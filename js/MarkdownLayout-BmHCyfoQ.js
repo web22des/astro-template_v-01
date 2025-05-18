f0883b08a2dabb2eb8018bf483f6b87d6664828c
web22des <web22des@users.noreply.github.com>
2025-05-18 21:17:03 +0000

    deploy: 9a9a0d36feb338912081399a463fecac854a99f9

---
+++ js/MarkdownLayout-BmHCyfoQ.js
@@ -0,0 +1,13 @@
+import { b as createAstro, c as createComponent, l as renderHead, r as renderComponent, n as renderSlot, a as renderTemplate } from './astro/server-BJsnRDlE.js';
+import 'kleur/colors';
+import { $ as $$Header, a as $$Footer } from './Footer-BI3xk6iA.js';
+
+const $$Astro = createAstro("https://web22des.github.io");
+const $$MarkdownLayout = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$MarkdownLayout;
+  const { frontmatter } = Astro2.props;
+  return renderTemplate`<html lang="ru" class="light"> <head><meta charset="utf-8"><!-- Базовые иконки --><link rel="icon" href="/favicon/favicon.ico" sizes="any"><link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml"><!-- Адаптивные иконки --><link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png"><link rel="manifest" href="/favicon/site.webmanifest"><title>${frontmatter.title}</title>${renderHead()}</head> <body class="wrapper"> ${renderComponent($$result, "Header", $$Header, {})} <article class="markdown__container page"> ${renderSlot($$result, $$slots["default"])} <!-- Сюда вставится содержимое .md --> </article> ${renderComponent($$result, "Footer", $$Footer, {})} </body></html>`;
+}, "/home/runner/work/astro-template_v-01/astro-template_v-01/src/layouts/MarkdownLayout.astro", void 0);
+
+export { $$MarkdownLayout as $ };
