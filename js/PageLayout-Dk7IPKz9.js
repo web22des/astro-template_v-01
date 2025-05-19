975c8886a1b59f7dd05bc1151b3146ec36fcd0ef
web22des <web22des@users.noreply.github.com>
2025-05-19 21:41:03 +0000

    deploy: 22c6740bfc407485888a75ca3ebbcb12c5f88c37

--- js/PageLayout-Dk7IPKz9.js
+++ js/PageLayout-Dk7IPKz9.js
@@ -1,7 +1,7 @@
 import { b as createAstro, c as createComponent, d as addAttribute, e as renderHead, r as renderComponent, f as renderSlot, a as renderTemplate } from './astro/server-jR75XK9s.js';
 import 'kleur/colors';
 /* empty css                           */
-import { $ as $$Header, a as $$Footer } from './Footer-axVH6Tdf.js';
+import { $ as $$Header, a as $$Footer } from './Footer-Ds17tMM-.js';
 
 const $$Astro = createAstro("https://web22des.github.io");
 const $$PageLayout = createComponent(($$result, $$props, $$slots) => {
